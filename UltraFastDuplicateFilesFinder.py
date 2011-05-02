119a2e64661579d88735f9abb3dc24077432a124
Tobias Schönit <tobias@tobias-K52Je.(none)>
2011-05-02 21:31:44 +0200

    Group by size added before calculating the hash. This has more speed

--- UltraFastDuplicateFilesFinder.py
+++ UltraFastDuplicateFilesFinder.py
@@ -30,6 +30,7 @@ this is public domain.
 import sys
 import os
 import hashlib
+import itertools
 
 # read one CHUNK_SIZE bytes to check duplicates
 CHUNK_SIZE = 1024
@@ -38,6 +39,11 @@ CHUNK_SIZE = 1024
 BUFFER_SIZE = 64*1024
 
 
+class FileInfo:
+    filename = ""
+    size = 0
+    hash = ""
+
 def get_file_hash(filename, limit_size=None, buffer_size=BUFFER_SIZE):
     """
     Return the md5 hash of given file as an hexadecimal string.
@@ -45,6 +51,7 @@ def get_file_hash(filename, limit_size=None, buffer_size=BUFFER_SIZE):
     limit_size can be used to read only the first n bytes of file.
     """
     # open file
+
     try:
         f = file(filename, "rb")
     except IOError:
@@ -67,84 +74,72 @@ def get_file_hash(filename, limit_size=None, buffer_size=BUFFER_SIZE):
     f.close()
     return hasher.hexdigest()
 
-
-filelist = []
+files = {}
 hashlist = {}
-hashcount = {}
-duplicates = []
-
-def check_file(filename):
-    """
-    Compare the given file to our lists of hashes
-    """    
-    # compute md5
-    h = get_file_hash(filename)
-    
-    # increase count
-    i = hashcount.get(h, 0)
-    hashcount[h] = i+1
-    
-    # store md5 and filename for later use
-    f = hashlist.get(h, [])
-    f.append(filename)
-    hashlist[h] = f
-
-
-
 totalsize = 0
 totalfiles = 0
-dupfiles = 0
-dupsize = 0
 
 def humanize_size(size):
     """
     Return the file size as a nice, readable string.
     """
-    for limit, suffix in ((1024**3, 'GiB'), (1024**2, 'MiB'), (1024, 'KiB')):
+    for limit, suffix in ((1024**3, 'GiB'), (1024**2, 'MiB'), (1024, 'KiB'), (1, 'B')):
         hsize = float(size) / limit
         if hsize > 0.5:
             return '%.2f %s' % (hsize, suffix)
 
-
-# we start here by checking all files
+# we start here by checking all filesizes
 for filename in sys.stdin:
     filename = filename.strip()
-
-    check_file(filename)
+    if not os.path.isfile(filename):
+        continue
+        
+    size = os.path.getsize(filename)
+    
+    fi = FileInfo()
+    fi.filename = filename
+    fi.size = size
+    files[filename] = fi
     totalfiles += 1
-    totalsize += os.path.getsize(filename)
+    totalsize += size
+    sys.stdout.write('%d files (%s)           \r' % (totalfiles, humanize_size(totalsize)))
 
+print ''
 # print the report
 print '%10s   %s' % ('size', 'filename')
 
-for h, f in hashlist.iteritems():
-    if hashcount[h] < 2:
-        # present one time, skip
+# group files by size
+selFsize = lambda fi:fi.size
+filenamesBySize = sorted(files.values(), key=selFsize)
+for k, g in itertools.groupby(filenamesBySize, selFsize):
+    filesOfThisSize = list(g)
+    if len(filesOfThisSize) <= 1:
         continue
-    
-    # reference file    
-    refsize = os.path.getsize(f[0])
-    refmd5 = get_file_hash(f[0])
-    print '%10d   %s' % (refsize, f[0])
-    
-    
-    for filename in f[1:]:
-        # and its copies
-        size = os.path.getsize(filename)
-        md5 = get_file_hash(filename)
+        
+    # calculate the hashes for this group
+    for fn in filesOfThisSize:     
+        h = get_file_hash(fn.filename)
+        if not hashlist.has_key(h):
+        	hashlist[h] = []
+        
+        fn.hash=h
+        hashlist[h].append(fn)
 
-        status = ' '
-        msg = ''
-        if md5 != refmd5:
-            status = '!'
-            msg = ' partial match only!'
-
-        print '%10d %s %s%s' % (size, status, filename, msg)
-        dupsize += size
-    dupfiles += 1
-    print
-
+nDupGroups = 0
+nDupFiles = 0
+sizeOfDups = 0
+for hl in hashlist:
+    fileinfos = hashlist[hl]
+    if len(fileinfos) > 1:
+        print 20 * '-'
+        
+        nDupGroups += 1  
+        nDupFiles += len(fileinfos)          
+        for fi in fileinfos:
+            sizeOfDups += fi.size
+            print '(%10s) %s' % (humanize_size(fi.size), fi.filename)
+         
 # final summary
-print '%d files checked (%s), %d duplicates (%s).' % (
-    totalfiles, humanize_size(totalsize), dupfiles, humanize_size(dupsize))
-
+print 20*'-'
+print 'found %d groups with %d duplicate files with a total size of %s' % \
+ (nDupGroups, nDupFiles, humanize_size(sizeOfDups))
